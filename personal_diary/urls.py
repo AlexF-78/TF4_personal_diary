a4ab5f53582f932e0ce33a3528d7cae738dfc391
алексей федулов <fab-78@mail.ru>
2026-02-05 01:23:30 +0200

    modified:   personal_diary/urls.py

--- personal_diary/urls.py
+++ personal_diary/urls.py
@@ -1,22 +1,24 @@
 """
-URL configuration for personal_diary project.
+Главный URL-конфигуратор проекта.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+Определяет маршруты для всего проекта, включая маршруты приложений
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Админ-панель Django
+    path("admin/", admin.site.urls),
+    # URL приложения дневника
+    path("", include("diary.urls")),
+    # URL для аутентификации (используем встроенные представления Django)
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
+
+# Маршруты для медиа-файлов в режиме отладки
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
